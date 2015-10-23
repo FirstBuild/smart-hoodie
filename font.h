60a7b6721e8b88da76a04940b45d5898c9342df3
Christopher M. Baker <mail.chris.baker@gmail.com>
2015-10-23 08:53:35 -0400

    v1.0.0: Initial commit.
    Receives text over bluetooth (must be null-terminated).
    Display is static if text can fit on the screen.
    Display is scrolled if text is larger than one screen width.
    Font file includes letters and a few symbols (numbers not included).

---
+++ font.h
@@ -0,0 +1,760 @@
+/**
+ * Copyright (c) 2015 FirstBuild
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#define LETTER_WIDTH(x) (sizeof(x) / sizeof(int))
+
+const PROGMEM int LETTER_A[] = {
+  0x000c,
+  0x0070,
+  0x0190,
+  0x0610,
+  0x0190,
+  0x0070,
+  0x000c
+};
+
+const PROGMEM int LETTER_B[] = {
+  0x07fc,
+  0x0444,
+  0x0444,
+  0x0444,
+  0x03b8
+};
+
+const PROGMEM int LETTER_C[] = {
+  0x03f8,
+  0x0404,
+  0x0404,
+  0x0404,
+  0x0404,
+  0x0208
+};
+
+const PROGMEM int LETTER_D[] = {
+  0x07fc,
+  0x0404,
+  0x0404,
+  0x0404,
+  0x0208,
+  0x01f0
+};
+
+const PROGMEM int LETTER_E[] = {
+  0x07fc,
+  0x0444,
+  0x0444,
+  0x0444,
+  0x0404
+};
+
+const PROGMEM int LETTER_F[] = {
+  0x07fc,
+  0x0440,
+  0x0440,
+  0x0440,
+  0x0400
+};
+
+const PROGMEM int LETTER_G[] = {
+  0x03f8,
+  0x0404,
+  0x0404,
+  0x0444,
+  0x0448,
+  0x027c
+};
+
+const PROGMEM int LETTER_H[] = {
+  0x07fc,
+  0x0040,
+  0x0040,
+  0x0040,
+  0x0040,
+  0x07fc
+};
+
+const PROGMEM int LETTER_I[] = {
+  0x07fc
+};
+
+const PROGMEM int LETTER_J[] = {
+  0x0018,
+  0x0004,
+  0x0004,
+  0x07f8
+};
+
+const PROGMEM int LETTER_K[] = {
+  0x07fc,
+  0x00c0,
+  0x0120,
+  0x0210,
+  0x0408,
+  0x0004
+};
+
+const PROGMEM int LETTER_L[] = {
+  0x07fc,
+  0x0004,
+  0x0004,
+  0x0004,
+  0x0004
+};
+
+const PROGMEM int LETTER_M[] = {
+  0x07fc,
+  0x0180,
+  0x0060,
+  0x0018,
+  0x0060,
+  0x0180,
+  0x07fc
+};
+
+const PROGMEM int LETTER_N[] = {
+  0x07fc,
+  0x0300,
+  0x00c0,
+  0x0020,
+  0x0018,
+  0x07fc
+};
+
+const PROGMEM int LETTER_O[] = {
+  0x03f8,
+  0x0404,
+  0x0404,
+  0x0404,
+  0x0404,
+  0x03f8
+};
+
+const PROGMEM int LETTER_P[] = {
+  0x07fc,
+  0x0440,
+  0x0440,
+  0x0440,
+  0x0440,
+  0x0380
+};
+
+const PROGMEM int LETTER_Q[] = {
+  0x03f8,
+  0x0404,
+  0x0404,
+  0x0414,
+  0x040c,
+  0x03fa
+};
+
+const PROGMEM int LETTER_R[] = {
+  0x07fc,
+  0x0440,
+  0x0440,
+  0x0440,
+  0x0440,
+  0x03bc
+};
+
+const PROGMEM int LETTER_S[] = {
+  0x0388,
+  0x0444,
+  0x0444,
+  0x0444,
+  0x0238
+};
+
+const PROGMEM int LETTER_T[] = {
+  0x0400,
+  0x0400,
+  0x07fc,
+  0x0400,
+  0x0400
+};
+
+const PROGMEM int LETTER_U[] = {
+  0x07f8,
+  0x0004,
+  0x0004,
+  0x0004,
+  0x0004,
+  0x07f8
+};
+
+const PROGMEM int LETTER_V[] = {
+  0x0600,
+  0x01c0,
+  0x0030,
+  0x000c,
+  0x0030,
+  0x01c0,
+  0x0600
+};
+
+const PROGMEM int LETTER_W[] = {
+  0x0600,
+  0x01c0,
+  0x0030,
+  0x000c,
+  0x0030,
+  0x01c0,
+  0x0030,
+  0x000c,
+  0x0030,
+  0x01c0,
+  0x0600
+};
+
+const PROGMEM int LETTER_X[] = {
+  0x060c,
+  0x0110,
+  0x00a0,
+  0x0040,
+  0x00a0,
+  0x0110,
+  0x060c
+};
+
+const PROGMEM int LETTER_Y[] = {
+  0x0600,
+  0x0100,
+  0x0080,
+  0x007c,
+  0x0080,
+  0x0100,
+  0x0600
+};
+
+const PROGMEM int LETTER_Z[] = {
+  0x040c,
+  0x0414,
+  0x0424,
+  0x0444,
+  0x0484,
+  0x0504,
+  0x0604
+};
+
+const PROGMEM int LETTER_a[] = {
+  0x0018,
+  0x00a4,
+  0x00a4,
+  0x00a4,
+  0x007c
+};
+
+const PROGMEM int LETTER_b[] = {
+  0x07fc,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x0078
+};
+
+const PROGMEM int LETTER_c[] = {
+  0x0078,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x0048
+};
+
+const PROGMEM int LETTER_d[] = {
+  0x0078,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x07fc
+};
+
+const PROGMEM int LETTER_e[] = {
+  0x0078,
+  0x00a4,
+  0x00a4,
+  0x00a4,
+  0x0068
+};
+
+const PROGMEM int LETTER_f[] = {
+  0x03fc,
+  0x0480
+};
+
+const PROGMEM int LETTER_g[] = {
+  0x0079,
+  0x0085,
+  0x0085,
+  0x0085,
+  0x00fe
+};
+
+const PROGMEM int LETTER_h[] = {
+  0x07fc,
+  0x0040,
+  0x0080,
+  0x0080,
+  0x007c
+};
+
+const PROGMEM int LETTER_i[] = {
+  0x04fc
+};
+
+const PROGMEM int LETTER_j[] = {
+  0x04ff
+};
+
+const PROGMEM int LETTER_k[] = {
+  0x07fc,
+  0x0020,
+  0x0050,
+  0x0088,
+  0x0004
+};
+
+const PROGMEM int LETTER_l[] = {
+  0x07fc
+};
+
+const PROGMEM int LETTER_m[] = {
+  0x00fc,
+  0x0080,
+  0x0080,
+  0x007c,
+  0x0080,
+  0x0080,
+  0x007c
+};
+
+const PROGMEM int LETTER_n[] = {
+  0x00fc,
+  0x0040,
+  0x0080,
+  0x0080,
+  0x007c
+};
+
+const PROGMEM int LETTER_o[] = {
+  0x0078,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x0078
+};
+
+const PROGMEM int LETTER_p[] = {
+  0x00ff,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x0078
+};
+
+const PROGMEM int LETTER_q[] = {
+  0x0078,
+  0x0084,
+  0x0084,
+  0x0084,
+  0x00ff
+};
+
+const PROGMEM int LETTER_r[] = {
+  0x00fc,
+  0x0080
+};
+
+const PROGMEM int LETTER_s[] = {
+  0x0048,
+  0x00a4,
+  0x0094,
+  0x0048
+};
+
+const PROGMEM int LETTER_t[] = {
+  0x03f8,
+  0x0084
+};
+
+const PROGMEM int LETTER_u[] = {
+  0x00f8,
+  0x0004,
+  0x0004,
+  0x0008,
+  0x00fc
+};
+
+const PROGMEM int LETTER_v[] = {
+  0x00c0,
+  0x0030,
+  0x000c,
+  0x0030,
+  0x00c0
+};
+
+const PROGMEM int LETTER_w[] = {
+  0x00f0,
+  0x000c,
+  0x0030,
+  0x00c0,
+  0x0030,
+  0x000c,
+  0x00f0
+};
+
+const PROGMEM int LETTER_x[] = {
+  0x00cc,
+  0x0030,
+  0x0030,
+  0x00cc
+};
+
+const PROGMEM int LETTER_y[] = {
+  0x0001,
+  0x00f1,
+  0x000e,
+  0x0008,
+  0x00f0
+};
+
+const PROGMEM int LETTER_z[] = {
+  0x008c,
+  0x0094,
+  0x00a4,
+  0x00c4
+};
+
+const PROGMEM int LETTER_SPACE[] = {
+  0x0000
+};
+
+const PROGMEM int LETTER_EXCLAMATION[] = {
+  0x07f4,
+};
+
+const PROGMEM int LETTER_HASH[] = {
+  0x0110,
+  0x07fc,
+  0x0110,
+  0x0110,
+  0x07fc,
+  0x0110,
+};
+
+const PROGMEM int LETTER_DOLLAR[] = {
+  0x0188,
+  0x0244,
+  0x07fe,
+  0x0224,
+  0x0118,
+};
+
+const PROGMEM int LETTER_PERCENT[] = {
+  0x0208,
+  0x0510,
+  0x0520,
+  0x0248,
+  0x0094,
+  0x0114,
+  0x0208,
+};
+
+const PROGMEM int LETTER_AMPERSAND[] = {
+  0x0338,
+  0x04c4,
+  0x0324,
+  0x0018,
+  0x0024,
+};
+
+const PROGMEM int LETTER_LPAREN[] = {
+  0x03fe,
+  0x0401,
+};
+
+const PROGMEM int LETTER_RPAREN[] = {
+  0x0401,
+  0x03fe,
+};
+
+const PROGMEM int LETTER_ASTERISK[] = {
+  0x0500,
+  0x0200,
+  0x0500,
+};
+
+const PROGMEM int LETTER_PLUS[] = {
+  0x0020,
+  0x0020,
+  0x00f8,
+  0x0020,
+  0x0020,
+};
+
+const PROGMEM int LETTER_COMMA[] = {
+  0x0002,
+  0x0004,
+};
+
+const PROGMEM int LETTER_DASH[] = {
+  0x0020,
+  0x0020,
+};
+
+const PROGMEM int LETTER_PERIOD[] = {
+  0x0004,
+};
+
+const PROGMEM int LETTER_FSLASH[] = {
+  0x000c,
+  0x0030,
+  0x00c0,
+  0x0700,
+};
+
+const PROGMEM int LETTER_COLON[] = {
+  0x0084,
+};
+
+const PROGMEM int LETTER_SEMICOLON[] = {
+  0x0002,
+  0x0084,
+};
+
+const PROGMEM int LETTER_EQUAL[] = {
+  0x0050,
+  0x0050,
+  0x0050,
+  0x0050,
+  0x0050,
+};
+
+const PROGMEM int LETTER_QUESTION[] = {
+  0x0200,
+  0x0400,
+  0x0434,
+  0x0440,
+  0x0380,
+};
+
+const PROGMEM int LETTER_AT[] = {
+  0x00f0,
+  0x030c,
+  0x0204,
+  0x0462,
+  0x0492,
+  0x0492,
+  0x04e2,
+  0x0212,
+  0x0310,
+  0x00f0,
+};
+
+const PROGMEM int LETTER_LBRACKET[] = {
+  0x07ff,
+  0x0401,
+};
+
+const PROGMEM int LETTER_BSLASH[] = {
+  0x0700,
+  0x00c0,
+  0x0030,
+  0x000c,
+};
+
+const PROGMEM int LETTER_RBRACKET[] = {
+  0x0401,
+  0x07ff,
+};
+
+const PROGMEM int LETTER_HAT[] = {
+  0x0200,
+  0x0400,
+  0x0800,
+  0x0400,
+  0x0200,
+};
+
+const PROGMEM int LETTER_UNDERSCORE[] = {
+  0x0001,
+  0x0001,
+  0x0001,
+  0x0001,
+  0x0001,
+  0x0001,
+};
+
+const PROGMEM int LETTER_TICK[] = {
+  0x0400,
+  0x0200,
+};
+
+const PROGMEM int LETTER_LBRACE[] = {
+  0x0040,
+  0x07bc,
+  0x0802,
+};
+
+const PROGMEM int LETTER_PIPE[] = {
+  0x07fe,
+};
+
+const PROGMEM int LETTER_RBRACE[] = {
+  0x0802,
+  0x07bc,
+  0x0040,
+};
+
+const PROGMEM int LETTER_TILDA[] = {
+  0x0100,
+  0x0200,
+  0x0200,
+  0x0100,
+  0x0100,
+  0x0200,
+};
+
+typedef struct font_entry_s {
+  char letter;
+  int width;
+  const int *const buffer;
+} font_entry_t;
+
+const font_entry_t FONT_ENTRIES[] = {
+  { 'A', LETTER_WIDTH(LETTER_A), LETTER_A },
+  { 'B', LETTER_WIDTH(LETTER_B), LETTER_B },
+  { 'C', LETTER_WIDTH(LETTER_C), LETTER_C },
+  { 'D', LETTER_WIDTH(LETTER_D), LETTER_D },
+  { 'E', LETTER_WIDTH(LETTER_E), LETTER_E },
+  { 'F', LETTER_WIDTH(LETTER_F), LETTER_F },
+  { 'G', LETTER_WIDTH(LETTER_G), LETTER_G },
+  { 'H', LETTER_WIDTH(LETTER_H), LETTER_H },
+  { 'I', LETTER_WIDTH(LETTER_I), LETTER_I },
+  { 'J', LETTER_WIDTH(LETTER_J), LETTER_J },
+  { 'K', LETTER_WIDTH(LETTER_K), LETTER_K },
+  { 'L', LETTER_WIDTH(LETTER_L), LETTER_L },
+  { 'M', LETTER_WIDTH(LETTER_M), LETTER_M },
+  { 'N', LETTER_WIDTH(LETTER_N), LETTER_N },
+  { 'O', LETTER_WIDTH(LETTER_O), LETTER_O },
+  { 'P', LETTER_WIDTH(LETTER_P), LETTER_P },
+  { 'Q', LETTER_WIDTH(LETTER_Q), LETTER_Q },
+  { 'R', LETTER_WIDTH(LETTER_R), LETTER_R },
+  { 'S', LETTER_WIDTH(LETTER_S), LETTER_S },
+  { 'T', LETTER_WIDTH(LETTER_T), LETTER_T },
+  { 'U', LETTER_WIDTH(LETTER_U), LETTER_U },
+  { 'V', LETTER_WIDTH(LETTER_V), LETTER_V },
+  { 'W', LETTER_WIDTH(LETTER_W), LETTER_W },
+  { 'X', LETTER_WIDTH(LETTER_X), LETTER_X },
+  { 'Y', LETTER_WIDTH(LETTER_Y), LETTER_Y },
+  { 'Z', LETTER_WIDTH(LETTER_Z), LETTER_Z },
+  { 'a', LETTER_WIDTH(LETTER_a), LETTER_a },
+  { 'b', LETTER_WIDTH(LETTER_b), LETTER_b },
+  { 'c', LETTER_WIDTH(LETTER_c), LETTER_c },
+  { 'd', LETTER_WIDTH(LETTER_d), LETTER_d },
+  { 'e', LETTER_WIDTH(LETTER_e), LETTER_e },
+  { 'f', LETTER_WIDTH(LETTER_f), LETTER_f },
+  { 'g', LETTER_WIDTH(LETTER_g), LETTER_g },
+  { 'h', LETTER_WIDTH(LETTER_h), LETTER_h },
+  { 'i', LETTER_WIDTH(LETTER_i), LETTER_i },
+  { 'j', LETTER_WIDTH(LETTER_j), LETTER_j },
+  { 'k', LETTER_WIDTH(LETTER_k), LETTER_k },
+  { 'l', LETTER_WIDTH(LETTER_l), LETTER_l },
+  { 'm', LETTER_WIDTH(LETTER_m), LETTER_m },
+  { 'n', LETTER_WIDTH(LETTER_n), LETTER_n },
+  { 'o', LETTER_WIDTH(LETTER_o), LETTER_o },
+  { 'p', LETTER_WIDTH(LETTER_p), LETTER_p },
+  { 'q', LETTER_WIDTH(LETTER_q), LETTER_q },
+  { 'r', LETTER_WIDTH(LETTER_r), LETTER_r },
+  { 's', LETTER_WIDTH(LETTER_s), LETTER_s },
+  { 't', LETTER_WIDTH(LETTER_t), LETTER_t },
+  { 'u', LETTER_WIDTH(LETTER_u), LETTER_u },
+  { 'v', LETTER_WIDTH(LETTER_v), LETTER_v },
+  { 'w', LETTER_WIDTH(LETTER_w), LETTER_w },
+  { 'x', LETTER_WIDTH(LETTER_x), LETTER_x },
+  { 'y', LETTER_WIDTH(LETTER_y), LETTER_y },
+  { 'z', LETTER_WIDTH(LETTER_z), LETTER_z },
+  { ' ', LETTER_WIDTH(LETTER_SPACE), LETTER_SPACE },
+  { '!', LETTER_WIDTH(LETTER_EXCLAMATION), LETTER_EXCLAMATION },
+  { '#', LETTER_WIDTH(LETTER_HASH), LETTER_HASH },
+  { '$', LETTER_WIDTH(LETTER_DOLLAR), LETTER_DOLLAR },
+  { '%', LETTER_WIDTH(LETTER_PERCENT), LETTER_PERCENT },
+  { '&', LETTER_WIDTH(LETTER_AMPERSAND), LETTER_AMPERSAND },
+  { '(', LETTER_WIDTH(LETTER_LPAREN), LETTER_LPAREN },
+  { ')', LETTER_WIDTH(LETTER_RPAREN), LETTER_RPAREN },
+  { '*', LETTER_WIDTH(LETTER_ASTERISK), LETTER_ASTERISK },
+  { '+', LETTER_WIDTH(LETTER_PLUS), LETTER_PLUS },
+  { ',', LETTER_WIDTH(LETTER_COMMA), LETTER_COMMA },
+  { '-', LETTER_WIDTH(LETTER_DASH), LETTER_DASH },
+  { '.', LETTER_WIDTH(LETTER_PERIOD), LETTER_PERIOD },
+  { '/', LETTER_WIDTH(LETTER_FSLASH), LETTER_FSLASH },
+  { ':', LETTER_WIDTH(LETTER_COLON), LETTER_COLON },
+  { ';', LETTER_WIDTH(LETTER_SEMICOLON), LETTER_SEMICOLON },
+  { '=', LETTER_WIDTH(LETTER_EQUAL), LETTER_EQUAL },
+  { '?', LETTER_WIDTH(LETTER_QUESTION), LETTER_QUESTION },
+  { '@', LETTER_WIDTH(LETTER_AT), LETTER_AT },
+  { '[', LETTER_WIDTH(LETTER_LBRACKET), LETTER_LBRACKET },
+  { '\\', LETTER_WIDTH(LETTER_BSLASH), LETTER_BSLASH },
+  { ']', LETTER_WIDTH(LETTER_RBRACKET), LETTER_RBRACKET },
+  { '^', LETTER_WIDTH(LETTER_HAT), LETTER_HAT },
+  { '_', LETTER_WIDTH(LETTER_UNDERSCORE), LETTER_UNDERSCORE },
+  { '`', LETTER_WIDTH(LETTER_TICK), LETTER_TICK },
+  { '{', LETTER_WIDTH(LETTER_LBRACE), LETTER_LBRACE },
+  { '|', LETTER_WIDTH(LETTER_PIPE), LETTER_PIPE },
+  { '}', LETTER_WIDTH(LETTER_RBRACE), LETTER_RBRACE },
+  { '~', LETTER_WIDTH(LETTER_TILDA), LETTER_TILDA }
+};
+
+const font_entry_t FONT_ENTRY_UNDEFINED = { 0, 0, 0 };
+
+#define MAX_LETTER_WIDTH 16
+
+class Font {
+  public:
+    static const font_entry_t * const getEntry(char c) {
+      int count = sizeof(FONT_ENTRIES) / sizeof(font_entry_t);
+
+      for (int i = 0; i < count; i++) {
+        const font_entry_t *const e = FONT_ENTRIES + i;
+
+        if (e->letter == c) {
+          return e;
+        }
+      }
+
+      return &FONT_ENTRY_UNDEFINED;
+    }
+
+    static int getWidth(char c) {
+      return getEntry(c)->width;
+    }
+
+    static const int *getBuffer(char c) {
+      static int buffer[MAX_LETTER_WIDTH];
+      const font_entry_t *const e = getEntry(c);
+
+      for (int i = 0; i < e->width; i++) {
+        buffer[i] = (int) pgm_read_word_near(e->buffer + i);
+      }
+      
+      return buffer;
+    }
+};
---
+++ hoodie.h
@@ -0,0 +1,240 @@
+/**
+ * Copyright (c) 2015 FirstBuild
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include "font.h"
+
+#ifndef HOODIE_H
+#define HOODIE_H
+
+#define HOODIE_VERSION_MAJOR 0
+#define HOODIE_VERSION_MINOR 0
+#define HOODIE_VERSION_BUILD 0
+
+#define PIN_R1    9
+#define PIN_OE    10
+#define PIN_ROW_A 11
+#define PIN_ROW_B 12
+#define PIN_ROW_C 13
+#define PIN_ROW_D A0
+#define PIN_G1    A1
+#define PIN_STB   A3
+#define PIN_CLK   A4
+
+#define HOODIE_SCREEN_WIDTH  32
+#define HOODIE_SCREEN_HEIGHT 16
+#define HOODIE_SCROLL_RATE   20
+
+#define HOODIE_SCREEN_WIDTH_IN_BYTES (HOODIE_SCREEN_WIDTH / 8)
+
+#define BLE_NAME "Hoodie"
+#define BLE_PACKET_SIZE 200
+#define BLE_PACKET_DELAY 5000
+
+class CHoodie {
+  private:
+    int _unlocked;
+    char _text[BLE_PACKET_SIZE + 2 * HOODIE_SCREEN_WIDTH];
+    unsigned long _textWidth;
+    unsigned long _columnOffset;
+    int _rowToDisplay;
+    char _displayBuffer[HOODIE_SCREEN_HEIGHT][HOODIE_SCREEN_WIDTH_IN_BYTES];
+    char _packetBuffer[BLE_PACKET_SIZE];
+    int _packetSize;
+    unsigned long _previousMilliseconds;
+    unsigned long _previousPacketMilliseconds;
+
+  public:
+    CHoodie(void) :
+      _unlocked(true),
+      _text(),
+      _textWidth(0),
+      _columnOffset(0),
+      _rowToDisplay(0),
+      _displayBuffer(),
+      _packetBuffer(),
+      _packetSize(0),
+      _previousMilliseconds(0),
+      _previousPacketMilliseconds(0) { }
+
+    void begin(void) {
+      pinMode(PIN_R1, OUTPUT);
+      pinMode(PIN_OE, OUTPUT);
+      pinMode(PIN_ROW_A, OUTPUT);
+      pinMode(PIN_ROW_B, OUTPUT);
+      pinMode(PIN_ROW_C, OUTPUT);
+      pinMode(PIN_ROW_D, OUTPUT);
+      pinMode(PIN_G1, INPUT);
+      pinMode(PIN_STB, OUTPUT);
+      pinMode(PIN_CLK, OUTPUT);
+
+      clear();
+
+      ble_set_name(BLE_NAME);
+      ble_begin();
+      delay(1000);
+    }
+
+    void loop(void) {
+      unsigned long current = millis();
+
+      if ((current - _previousMilliseconds) > HOODIE_SCROLL_RATE) {
+        _previousMilliseconds = current;
+
+        if (_textWidth > HOODIE_SCREEN_WIDTH) {
+          _columnOffset = (_columnOffset + 1) % _textWidth;
+          updateDisplay();
+        }
+      }
+      
+      if (ble_available()) {
+        if ((current - _previousPacketMilliseconds) > BLE_PACKET_DELAY) {
+          _previousPacketMilliseconds = current;
+          _packetSize = 0;
+        }
+        
+        while (ble_available()) {
+          char c = ble_read();
+
+          if (c == 0) {
+            _packetBuffer[_packetSize] = 0;
+            setText(_packetBuffer);
+          }
+          else if (_packetSize < sizeof(_packetBuffer)) {
+            _packetBuffer[_packetSize++] = c;
+          }
+        }
+      }
+
+      ble_do_events();
+    }
+
+    void tick(void) {
+      for (int i = 0; i < HOODIE_SCREEN_WIDTH_IN_BYTES; i++) {
+        shiftOut(PIN_R1, PIN_CLK, MSBFIRST, _displayBuffer[_rowToDisplay][i]);
+      }
+
+      digitalWrite(PIN_OE, 1);
+      digitalWrite(PIN_ROW_A, _rowToDisplay & 1);
+      digitalWrite(PIN_ROW_B, _rowToDisplay & 2);
+      digitalWrite(PIN_ROW_C, _rowToDisplay & 4);
+      digitalWrite(PIN_ROW_D, _rowToDisplay & 8);
+      digitalWrite(PIN_STB, 0);
+      digitalWrite(PIN_STB, 1);
+      digitalWrite(PIN_OE, 0);
+
+      _rowToDisplay = (_rowToDisplay + 1) % HOODIE_SCREEN_HEIGHT;
+    }
+
+    void clear(void) {
+      memset(_displayBuffer, 0xff, sizeof(_displayBuffer));
+    }
+
+    void setPixel(int row, int column, int on) {
+      int index = HOODIE_SCREEN_WIDTH_IN_BYTES - (column / 8) - 1;
+      int offset = column % 8;
+      int mask = 1 << offset;
+
+      if (row < HOODIE_SCREEN_HEIGHT) {
+        if (column < HOODIE_SCREEN_WIDTH) {
+          if (on) {
+            _displayBuffer[row][index] &= ~mask;
+          }
+          else {
+            _displayBuffer[row][index] |= mask;
+          }
+        }
+      }
+    }
+
+    void setColumn(int column, int pixels) {
+      for (int i = 0; i < HOODIE_SCREEN_HEIGHT; i++) {
+        setPixel(i, column, pixels & (1 << i));
+      }
+    }
+
+    void addLetter(unsigned long& column, const int *buffer, int width) {
+      for (int i = 0; i < width; i++, column++) {
+        int offset = column - _columnOffset;
+        
+        if (column < _columnOffset) {
+          /* skip */
+        }
+        else if (offset > HOODIE_SCREEN_WIDTH) {
+          break;
+        }
+        else {
+          setColumn(offset, buffer[i]);
+        }
+      }
+    }
+
+    void updateDisplay(void) {
+      unsigned long c = 0;
+      int space = 0;
+
+      for (char *text = _text; *text; text++) {
+        int width = Font::getWidth(*text);
+        const int *buffer = Font::getBuffer(*text);
+
+        addLetter(c, buffer, width);
+        addLetter(c, &space, 1);
+      }
+    }
+
+    static int getWidth(const char *text) {
+      unsigned long w;
+      
+      for (w = 0; *text; text++) {
+        w += Font::getWidth(*text) + 1;
+      }
+
+      return w;
+    }
+
+    void setText(const char *text) {
+      _columnOffset = 0;
+      _textWidth = getWidth(text);
+
+      if (_textWidth > HOODIE_SCREEN_WIDTH) {
+        int textLength = strlen(text);
+
+        _textWidth += 2 * HOODIE_SCREEN_WIDTH;
+        
+        memset(_text, ' ', HOODIE_SCREEN_WIDTH);
+        memcpy(_text + HOODIE_SCREEN_WIDTH, text, textLength);
+        memset(_text + HOODIE_SCREEN_WIDTH + textLength, ' ', HOODIE_SCREEN_WIDTH);
+        _text[2 * HOODIE_SCREEN_WIDTH + textLength] = 0;
+
+      }
+      else {
+        strncpy(_text, text, sizeof(_text));
+      }
+
+      clear();
+      updateDisplay();
+    }
+};
+
+CHoodie Hoodie;
+
+#endif /* HOODIE_H */
